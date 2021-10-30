360570062d1451f2700b90598aab175d6554cd82
lopezrunco <lopezrunco@gmail.com>
2021-10-30 12:57:59 -0300

    Controlador Creacion de tareas

--- controllers/todos/create.js
+++ controllers/todos/create.js
@@ -1,5 +1,31 @@
+const todoModel = require('../../models/todo')
+
 module.exports = (sequelize) => {
     return (request, response) => {
-        // TODO: Crear un todo
+
+        // Guarda el body en una variable
+        const todo = request.body
+
+        // Llama la funcion create del modelo pasandole los valores que vienen en la request
+        todoModel(sequelize).create({
+            title: todo.title,
+            description: todo.description,
+            priority: todo.priority,
+            completed: todo.completed
+
+        // Responde al front-end con la tarea creada
+        }).then(todo => {
+            response.json({
+                todo
+            })
+        
+        // Logueo de error y mensaje
+        }).catch(error => {
+            console.error(error)
+
+            response.status(500).json({
+                message: 'Error al crear la tarea'
+            })
+        })
     }
 }
