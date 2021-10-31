59ff07d447ffc21ddfd65e57cea39c4de0f2a719
lopezrunco <lopezrunco@gmail.com>
2021-10-30 22:15:49 -0300

    Paginado en Controlador Mostrar tarea por ID

--- controllers/todos/get-all.js
+++ controllers/todos/get-all.js
@@ -3,8 +3,20 @@ const todoModel = require('../../models/todo')
 module.exports = (sequelize) => {
     return (request, response) => {
 
+        // Definicion de un objeto vacio para la paginacion
+        const pagination = {}
+
+        // Este codigo correra solo si se estan mandando los parametros page & itemsPerPage
+        if (request.query.page && request.query.itemsPerPage) {
+
+            // Definicion de offset (items que se saltaran al mostrar) 
+            pagination.offset = (request.query.page - 1) * request.query.itemsPerPage
+            // Definicion de items a mostrar por pagina
+            pagination.limit = parseInt(request.query.itemsPerPage)
+        }
+
         // Lista todos las tareas y responde mostrandolas en la UI
-        todoModel(sequelize).findAll().then(todos => {
+        todoModel(sequelize).findAll(pagination).then(todos => {
             response.status(200).json({
                 todos
             })
